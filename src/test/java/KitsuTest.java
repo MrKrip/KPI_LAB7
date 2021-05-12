cf0be2d4c18722bd4a572e7ccbab45aa580c4106
MrKrip <45828333+MrKrip@users.noreply.github.com>
2021-05-12 18:28:32 +0300

    Ah shit, here we go again

---
+++ src/test/java/KitsuTest.java
@@ -0,0 +1,65 @@
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.RestAssured.given;
+
+@RunWith(SerenityRunner.class)
+public class KitsuTest {
+
+    @Test
+    public void GetAnimeList()
+    {
+        given()
+                .baseUri(Config.Kitsu_BASE_URL)
+                .when()
+                .get(Config.Anime_List)
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public void GetAnimeById()
+    {
+        given()
+                .baseUri(Config.Kitsu_BASE_URL)
+                .pathParam("animeId","1")
+                .when()
+                .get(Config.Anime_By_Id)
+                .then()
+                .log().body()
+                .assertThat()
+                .body(Matchers.notNullValue());
+    }
+
+    @Test
+    public void GetAnimeByFakeId()
+    {
+        given()
+                .baseUri(Config.Kitsu_BASE_URL)
+                .pathParam("animeId","1234567890")
+                .when()
+                .get(Config.Anime_By_Id)
+                .then()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    public void DeletePost()
+    {
+        given()
+                .baseUri(Config.Kitsu_BASE_URL)
+                .pathParam("animeId","167585")
+                .when()
+                .delete(Config.Delete_Post_By_Id)
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+}
